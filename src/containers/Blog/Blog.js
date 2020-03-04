e8c8aed8326d6b910bc5fe9afff2443ad5996da7
johnny055279 <johnny055279@gmail.com>
2020-03-04 10:29:15 +0800

    create fake data

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,18 +1,51 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
+
+    state = {
+        posts: []
+    }
+
+    componentDidMount() {
+
+        // get the http request to the url
+        // Axios use 'promises' and GET returns a promises, and we can use 'then()' which takes a function as the input and will executed once
+        // the promises resolves.
+        axios.get('https://jsonplaceholder.typicode.com/posts').then(response => {
+
+            // show only 4 data on the page.
+            const posts = response.data.slice(0, 4);
+
+
+            // distribute the author to the posts
+            const updatePosts = posts.map(post => {
+                return {
+                    ...post,
+                    author: 'Max',
+                }
+            });
+
+            // setState need write here because axios.get() only send the request to the server, it won't wait the fetch data come back if you write the setState outside the function.
+            this.setState({posts: updatePosts})
+            console.log(response)
+        });
+    }
+
     render () {
+
+        const posts = this.state.posts.map(post => {
+            return <Post key = {post.id} title = {post.title} author = {post.author}/>
+        })
+
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {posts}
                 </section>
                 <section>
                     <FullPost />
